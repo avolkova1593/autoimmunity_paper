e3feefd5cecd0a9d65b315abeb885972dc743060
Angelina Volkova <angelina.volkova@nyumc.org>
2021-02-06 16:08:27 -0500

    Update qiime2_train_taxonomy.sh

--- qiime2_train_taxonomy.sh
+++ qiime2_train_taxonomy.sh
@@ -1,7 +1,7 @@
 ###Written by Angelina Volkova on 05/05/2020
 ###This script trains a taxonomy classifier on
 ###whole 16s rRNA sequences from GreenGenes 13_8 database
-
+#!/bin/bash
 output="output_directory"
 database_seqs=$output/gg_otu_99.qza
 database_tax=$output/ref-taxonomy.qza
